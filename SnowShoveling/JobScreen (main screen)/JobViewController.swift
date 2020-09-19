7063493b9adb3629091524edd6c12161ad9d847c
Sheen Patel <programmingpatel@gmail.com>
2020-09-19 16:49:15 -0400

    added user location tracking and permissions functionality

--- SnowShoveling/JobScreen (main screen)/JobViewController.swift
+++ SnowShoveling/JobScreen (main screen)/JobViewController.swift
@@ -14,31 +14,47 @@ import MapKit
 
 class JobViewController: UIViewController {
 
-       
+    let buttonBorderWidth:CGFloat = 3;
+    let buttonCornerRadius:CGFloat = 15;
+    
+    //the next two are for the map. (see ./JobViewControllerMapView.swift)
 	@IBOutlet var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
 	//@IBOutlet var jobTableView: UITableView!
     @IBOutlet weak var profileButton: UIButton!
     @IBOutlet weak var addJobButton: UIButton!
     @IBOutlet weak var menuButton: UIButton!
+    @IBOutlet weak var centerLocationButton: UIButton!
     
+    var isTrackingUserLocation:Bool = false;
     //var hasLoaded = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //remove the navigation bar for this un
-        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        self.navigationController?.setNavigationBarHidden(true, animated: false);
         
         //circlurize you button and filter button
-        addJobButton.layer.cornerRadius = 15
+        addJobButton.layer.cornerRadius = buttonCornerRadius;
         
-        menuButton.layer.cornerRadius = 15
-        menuButton.layer.borderWidth = 3
-        profileButton.layer.cornerRadius = 15
-        profileButton.layer.borderWidth = 3
+        menuButton.layer.cornerRadius = buttonCornerRadius;
+        menuButton.layer.borderWidth = buttonBorderWidth;
+        profileButton.layer.cornerRadius = buttonCornerRadius;
+        profileButton.layer.borderWidth = buttonBorderWidth;
+        centerLocationButton.layer.cornerRadius = buttonCornerRadius;
+        centerLocationButton.layer.borderWidth = buttonBorderWidth;
         
         profileButton.layer.borderColor = UIColor(red: 123, green: 153, blue: 130, alpha: 1).cgColor
         menuButton.layer.borderColor = UIColor(red: 123, green: 153, blue: 130, alpha: 1).cgColor
+        
+        //user location stuff
+        checkLocationServices()
+        
+        if !checkLocationAuthorization() {
+            centerLocationButton.isHidden = true;
+        }
         //login not neccesary b/c user stays logged in.
 //        logIn: if UserDefaults.standard.bool(forKey: "isLoggedIn") {
 //            guard let email = KeychainWrapper.standard.string(forKey: "email"), let password = KeychainWrapper.standard.string(forKey: "password") else {
@@ -70,6 +86,20 @@ class JobViewController: UIViewController {
         if segue.identifier == "showDetail" {
             let detailViewController = segue.destination as! DetailViewController
             detailViewController.jobToDisplay = (sender as! Job)
+        }
+    }
+    
+    @IBAction func centerLocationButton(_ sender: UIButton) {
+        if checkLocationAuthorization() {
+            centerOnUserLocation()
+            if isTrackingUserLocation {
+                isTrackingUserLocation.toggle();
+                //sender.currentImage = UIImage(contentsOfFile: "location");
+                mapView.userTrackingMode = .none;
+            } else {
+                isTrackingUserLocation.toggle();
+                mapView.userTrackingMode = .follow;
+            }
         }
     }
     
--- SnowShoveling/JobScreen (main screen)/JobViewControllerMapView.swift
+++ SnowShoveling/JobScreen (main screen)/JobViewControllerMapView.swift
@@ -9,8 +9,9 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-extension JobViewController {
+extension JobViewController {    
 	func reloadMap() {        
         //iterate through the array of jobs and place a marker for each coordinate
         for job in FirebaseService.shared.jobArray {
@@ -33,4 +34,76 @@ extension JobViewController {
             }
         }
 	}
+    
+    func checkLocationAuthorization() -> Bool {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways, .authorizedWhenInUse:
+            mapView.showsUserLocation = true;
+            return true;
+        case .denied:
+            //show alert show how to change
+            showLocationError()
+            break;
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            break;
+        case .restricted:
+            //show alert letting know how to change.
+            showLocationError()
+            break;
+        default:
+            print("Unknown authorization.")
+            let alert = UIAlertController(title: "pester the developer", message: "It seems that the location manager got updated and something broke! ANNOY the developers to get this fixed.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("I'll annoy him", comment: "Default action"), style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+        return false;
+    }
+    
+    func centerOnUserLocation()  {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: 1000, longitudinalMeters: 1000)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    private func showLocationError() {
+        //give the user an error that their location services are disabled.
+        let alert = UIAlertController(title: "Location Services Disabled", message: "Yardies needs your location to find jobs near you and to create jobs. Go into location settings to enable.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Ok", comment: "Dismiss the popup."), style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            //do stuff when location active
+            setupLocationManager()
+            _ = checkLocationAuthorization()
+        } else {
+            showLocationError()
+        }
+    }
+}
+
+///This extension deals with showing the user's location on the screen.
+extension JobViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //this function happens anytime the user's location changes.
+    }
+    
+    //called anytime the location authorization changes.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if checkLocationAuthorization() {
+            mapView.showsUserLocation = true;
+        } else {
+            mapView.showsUserLocation = false;
+        }
+    }
+    
+    
 }
